b59146946901876486760dcbc3017756ec485005
gjoko.kostadinov <gjoko.kostadinov@musala.com>
2016-03-06 16:15:04 +0100

    Deleted unused stuff

--- src/org/gjoko/javabrains/jdbcDemo.java
+++ src/org/gjoko/javabrains/jdbcDemo.java
@@ -1,10 +1,8 @@
 package org.gjoko.javabrains;
 
-import org.gjoko.javabrains.dao.HibernateDaoImpl;
 import org.gjoko.javabrains.dao.NamedParameterJdbcTemplateImpl;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
 public class jdbcDemo {
 
